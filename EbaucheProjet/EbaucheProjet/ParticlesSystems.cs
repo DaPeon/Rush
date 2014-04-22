32fcebb24838db9e7f8fb01f6b748ae8f8809c4b
TIBI <NicolasTibi@yahoo.fr>
2014-04-22 02:06:36 +0200

    TODO : DODO
    Bonne nuit internet
    Fer dodo

---
+++ EbaucheProjet/EbaucheProjet/ParticlesSystems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EbaucheProjet
+{
+    // ADD PARTICLES SYSTEMS
+    //
+    // Blood (sweat & tears)
+    // Fire
+    // Explosions
+    // Smoke
+    // Rain
+    // Dust
+    // Bullets
+    // Teleportation particles
+    // Electro
+    // Much much more
+}
